17728a43250dcce2ae6a2741613dce7eba21b5ed
dinoooop <dinoooop@gmail.com>
2024-09-26 14:14:35 +0530

    run added

---
+++ extensions/gemini-8000/src/run.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect } from 'vitest';
+import { run } from '.';
+import { FunctionRunResult } from '../generated/api';
+
+describe('cart transform function', () => {
+  it('returns no operations', () => {
+    
+    const result = run({
+      cart: {
+        lines: []
+      }
+    });
+    const expected: FunctionRunResult = { operations: [] };
+
+    expect(result).toEqual(expected);
+  });
+});
