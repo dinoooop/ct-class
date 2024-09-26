2603887e67f25215c4fb0a07977cbae069fef9a6
dinoooop <dinoooop@gmail.com>
2024-09-26 14:09:16 +0530

    import

---
+++ extensions/gemini-8000/src/index.ts
@@ -0,0 +1,9 @@
+
+
+// The function to run whenever the cart changes
+export const run: any = ({ cart }:any) => {
+  // Log "Hello World" to the console when the cart changes
+  console.log("Hello World");
+  // Return an empty result since we're not applying any changes
+  return {};
+};
